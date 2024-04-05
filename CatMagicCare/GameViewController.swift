c7f7ca0f52381a7014a51564f42743e7a26c0883
Victor Fernandez <victor.fernandeziii@gmail.com>
2024-04-05 18:56:35 -0400

    Added some objects

--- CatMagicCare/GameViewController.swift
+++ CatMagicCare/GameViewController.swift
@@ -14,26 +14,23 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
+        // load the scene from the GameScene.sks
         if let scene = GKScene(fileNamed: "GameScene") {
             
-            // Get the SKScene from the loaded GKScene
+            // cast the top-level node within the scene as a GameScene object
             if let sceneNode = scene.rootNode as! GameScene? {
                 
                 // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
+                //sceneNode.entities = scene.entities
+                //sceneNode.graphs = scene.graphs
                 
                 // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
+                //sceneNode.scaleMode = .aspectFill
                 
                 // Present the scene
                 if let view = self.view as! SKView? {
                     view.presentScene(sceneNode)
-                    
                     view.ignoresSiblingOrder = true
-                    
                     view.showsFPS = true
                     view.showsNodeCount = true
                 }
