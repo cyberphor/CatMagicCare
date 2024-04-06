50f04fdb0c37bcf2241539b70ed25b0ae0dd0d92
Victor Fernandez <victor.fernandeziii@gmail.com>
2024-04-05 22:27:15 -0400

    Added a function

--- CatMagicCare/GameScene.swift
+++ CatMagicCare/GameScene.swift
@@ -10,14 +10,23 @@ import SpriteKit
 class GameScene: SKScene { // SpriteKitScene
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
-        //background.size = self.size
-        //background.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
         background.zPosition = 0
         self.addChild(background)
         
-        let player = SKSpriteNode(imageNamed: "player")
-        //player.position = CGPoint(x: self.size.width, y: self.size.height/5)
-        player.zPosition = 2
-        self.addChild(player)
+        //let player = SKSpriteNode(imageNamed: "player")
+        //player.zPosition = 2
+        //player.position = CGPoint(x: 0, y: 0)
+        //self.addChild(player)
+    }
+    
+    func eatCookie() {
+        let cookie = SKSpriteNode(imageNamed: "player")
+        cookie.zPosition = 1
+        cookie.position = CGPoint(x: 0, y: 0)
+        self.addChild(cookie)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        eatCookie()
     }
 }
